286475681ad37e6e89452edbf871972e887c0cc4
LasCreations <lascellemckenzie@gmail.com>
2023-01-27 13:34:51 -0500

    Shift Works

--- GuiCalculator.c
+++ GuiCalculator.c
@@ -1,5 +1,48 @@
 //Creating a scientific calculator
 
+
+/*
+	Key functions to check over 
+	g_print("%s\n",equation);
+
+	GDK_BUTTON_PRESS
+	GDK_BUTTON_RELEASE
+	GDK_BUTTON_PRESS
+	GDK_2BUTTON_PRESS
+	GDK_BUTTON_RELEASE
+
+	gtk_label_set_text(GTK_LABEL(label[0]),"Wah gwan");
+	
+	view[0] = gtk_text_view_new(); 
+	view[1] = gtk_text_view_new();
+
+
+	gtk_widget_set_visible(label[2],false);
+	gtk_widget_set_visible(label[3],false);
+	
+
+	gtk_text_view_set_editable(GTK_TEXT_VIEW(view[0]),false);
+	gtk_text_view_set_editable(GTK_TEXT_VIEW(view[1]),false);
+	
+	To Change Color of view
+	GdkRGBA text_color;
+	gdk_rgba_parse (&text_color, "white");
+	gtk_widget_override_color (box, GTK_STATE_FLAG_INSENSITIVE, &text_color);
+	
+	
+	gtk_widget_set_sensitive(GTK_WIDGET(view[0]),FALSE);
+	gtk_widget_set_sensitive(GTK_WIDGET(view[1]),FALSE);
+	
+	⁰ ¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹ ⁺ ⁻ ⁼ ⁽ ⁾  
+	₀ ₁ ₂ ₃ ₄ ₅ ₆ ₇ ₈ ₉ ₊ ₋ ₌ ₍ ₎  
+	ᵃ ᵇ ᶜ ᵈ ᵉ ᶠ ᵍ ʰ ⁱ ʲ ᵏ ˡ ᵐ ⁿ ᵒ ᵖ ʳ ˢ ᵗ ᵘ ᵛ ʷ ˣ ʸ ᶻ  
+	ᴬ ᴮ ᴰ ᴱ ᴳ ᴴ ᴵ ᴶ ᴷ ᴸ ᴹ ᴺ ᴼ ᴾ ᴿ ᵀ ᵁ ⱽ ᵂ  
+	ₐ ₑ ₕ ᵢ ⱼ ₖ ₗ ₘ ₙ ₒ ₚ ᵣ ₛ ₜ ᵤ ᵥ ₓ 
+	ᵅ ᵝ ᵞ ᵟ ᵋ ᶿ ᶥ ᶲ ᵠ ᵡ  
+	ᵦ ᵧ ᵨ ᵩ ᵪ   
+*/
+
+
 #include<math.h> 
 #include<stddef.h>
 #include<stdint.h> 
@@ -12,28 +55,23 @@
 typedef struct{
 	GtkWidget *window;
 	GtkWidget *grid;
-	GtkWidget *button[45];
-	//GtkWidget *ShiftLabel[];
-	
+	GtkWidget *button[45];	
 }calc;
 
-
-bool Is_Shift_Clicked = false;
 GtkWidget *box;
-GtkWidget *label[2];
+GtkWidget *label[4];
 GtkWidget *ButtonLabel[24];
 char *buffer = NULL;
 
-//Return a string or pointer
+bool Is_Shift_Active;
+
 static void SplitEquation(char *equation){
-	//g_print("%s\n",equation);
-	for(size_t i=0;i<strlen(equation);i++){
-		
+	for(size_t i=0;i<strlen(equation);i++){	
 		if(isdigit(equation[i])){
-			
+		
 		}
 		else if(isalpha(equation[i])){
-			
+
 		}
 	}
 }
@@ -63,27 +101,28 @@ static void ClearBuffer(){
 	gtk_entry_set_text(GTK_ENTRY(box), " ");
 }
 
-
 gboolean Shift_Clicked(GtkWidget *widget, GdkEventButton *event, gpointer user_data){
-	/*
-	GDK_BUTTON_PRESS
-	GDK_BUTTON_RELEASE
-	GDK_BUTTON_PRESS
-	GDK_2BUTTON_PRESS
-	GDK_BUTTON_RELEASE
-	*/
-
-	gtk_widget_set_visible(label[0],false);
-	//gtk_label_set_text(GTK_LABEL(label[0]),"Wah gwan");
-	
+		
 	if(event->type == GDK_BUTTON_PRESS){
-		g_print("Shift Clicked\n");		
-	}
+		switch(Is_Shift_Active){
+			case true:
+				//HideShift();
+				gtk_widget_hide(label[2]);
+				Is_Shift_Active = false;
+				break;
+			case false:
+				//ShowShift();
+				gtk_widget_show(label[2]);
+				Is_Shift_Active = true;
+				break;
+		}	
+	}	
 	return TRUE;
-}	
+}
+
+
 
 static void GetInput(GtkButton *button, gpointer data){
-
 	const gchar* text = gtk_button_get_label(button);
 
 	/*Dynamically allocating the memory for the string
@@ -92,8 +131,7 @@ static void GetInput(GtkButton *button, gpointer data){
 	If the buffer is not empty then we reallocate space to the memory of the buffer by adding the 
 	current size of the buffer to the current size of the preceeding text.
 	If C is pressed the we free the memory and then set the buffer back to NULL
-	and output an empty screen*/
-	
+	and output an empty screen*/	
 	if(strcmp("ON/CLR",text)==0 || strcmp("AC",text)==0){
 		ClearBuffer();
 	}
@@ -111,7 +149,6 @@ static void GetInput(GtkButton *button, gpointer data){
 }
 
 static void activate(GtkApplication *app, gpointer user_data){
-	
 	calc widget;
 
 	widget.window = gtk_application_window_new(app);
@@ -122,13 +159,13 @@ static void activate(GtkApplication *app, gpointer user_data){
 
 	widget.grid = gtk_grid_new();
 	gtk_container_add(GTK_CONTAINER(widget.window),widget.grid);
-	
-	//view[0] = gtk_text_view_new(); 
-	//view[1] = gtk_text_view_new(); 
+
 	box= gtk_entry_new();
-	label[0] = gtk_label_new ("quit");
+
+	label[0] = gtk_label_new (" ");
 	label[1] = gtk_label_new ("0.");
-	
+	label[2] = gtk_label_new ("🅂");
+	label[3] = gtk_label_new ("🄰");
 
 	ButtonLabel[0] = gtk_label_new ("x!");
 	ButtonLabel[1] = gtk_label_new ("nPr");
@@ -156,42 +193,8 @@ static void activate(GtkApplication *app, gpointer user_data){
 	ButtonLabel[23] = gtk_label_new ("%");
 
 
-
-	//gtk_widget_set_visible(label[0],false);
-	
 	gtk_editable_set_editable(GTK_EDITABLE(box), FALSE);
-	//gtk_text_view_set_editable(GTK_TEXT_VIEW(view[0]),false);
-	//gtk_text_view_set_editable(GTK_TEXT_VIEW(view[1]),false);
-
-	//To Change Color of view
-	/*
-	GdkRGBA text_color;
-	gdk_rgba_parse (&text_color, "white");
-	gtk_widget_override_color (box, GTK_STATE_FLAG_INSENSITIVE, &text_color);
-	*/
-	
-	//gtk_widget_set_sensitive(GTK_WIDGET(view[0]),FALSE);
-	//gtk_widget_set_sensitive(GTK_WIDGET(view[1]),FALSE);
 	gtk_widget_set_sensitive(GTK_WIDGET(box),FALSE);
-
-
-	/*
-	⁰ ¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹ ⁺ ⁻ ⁼ ⁽ ⁾  
-	₀ ₁ ₂ ₃ ₄ ₅ ₆ ₇ ₈ ₉ ₊ ₋ ₌ ₍ ₎  
- 
-	ᵃ ᵇ ᶜ ᵈ ᵉ ᶠ ᵍ ʰ ⁱ ʲ ᵏ ˡ ᵐ ⁿ ᵒ ᵖ ʳ ˢ ᵗ ᵘ ᵛ ʷ ˣ ʸ ᶻ  
- 
-	ᴬ ᴮ ᴰ ᴱ ᴳ ᴴ ᴵ ᴶ ᴷ ᴸ ᴹ ᴺ ᴼ ᴾ ᴿ ᵀ ᵁ ⱽ ᵂ  
- 
-	ₐ ₑ ₕ ᵢ ⱼ ₖ ₗ ₘ ₙ ₒ ₚ ᵣ ₛ ₜ ᵤ ᵥ ₓ 
- 
-	ᵅ ᵝ ᵞ ᵟ ᵋ ᶿ ᶥ ᶲ ᵠ ᵡ  
- 
-	ᵦ ᵧ ᵨ ᵩ ᵪ   
-
-	*/
-
-	//Add text to the buttons and attach them to the grid
 	
 	widget.button[0] = gtk_button_new_with_label("0");
 	widget.button[1] = gtk_button_new_with_label("1");
@@ -241,44 +244,34 @@ static void activate(GtkApplication *app, gpointer user_data){
 	widget.button[45] = gtk_button_new_with_label("EXP");
 
 	/*  (X-Position, Y-Position, X-Span, Y-Span)  */
-
-	//First
-	gtk_grid_attach(GTK_GRID(widget.grid),label[0],0,0,6,1);
-	
-	//Second
+	//First Row
+	gtk_grid_attach(GTK_GRID(widget.grid),label[0],0,0,1,1);
+	gtk_grid_attach(GTK_GRID(widget.grid),label[2],1,0,1,1);
+	gtk_grid_attach(GTK_GRID(widget.grid),label[3],2,0,1,1);
+	//Second Row
 	gtk_grid_attach(GTK_GRID(widget.grid),box,0,1,6,1);
-	
-	//Third
+	//Third Row
 	gtk_grid_attach(GTK_GRID(widget.grid),label[1],4,2,2,1);
-
-	//Fourth
+	//Fourth Row
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[17],0,3,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[18],1,3,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[19],2,3,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[15],3,3,1,1);
-	
-	//Fifth
+	//Fifth Row
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[0],0,4,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[1],1,4,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[2],2,4,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[3],3,4,1,1);
-		
-
 	//Sixth Row
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[20],0,5,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[21],1,5,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[22],2,5,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[23],3,5,1,1);
-	
 	//Seventh Row
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[4],0,6,1,1);
-	//gtk_grid_attach(GTK_GRID(widget.grid)," ",1,4,1,1);
-	//gtk_grid_attach(GTK_GRID(widget.grid)," ",2,4,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[5],3,6,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[6],4,6,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[7],5,6,1,1);
-
-
 	//Eigth Row
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[24],0,7,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[25],1,7,1,1);
@@ -286,7 +279,6 @@ static void activate(GtkApplication *app, gpointer user_data){
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[27],3,7,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[28],4,7,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[29],5,7,1,1);
-
 	//Ninth Row
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[8],0,8,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[9],1,8,1,1);
@@ -294,7 +286,6 @@ static void activate(GtkApplication *app, gpointer user_data){
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[11],3,8,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[12],4,8,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[13],5,8,1,1);
-
 	//Tenth Row
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[30],0,9,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[31],1,9,1,1);
@@ -302,13 +293,11 @@ static void activate(GtkApplication *app, gpointer user_data){
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[33],3,9,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[34],4,9,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[35],5,9,1,1);
-
 	//Eleventh Row
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[14],0,10,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[15],3,10,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[16],4,10,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[17],5,10,1,1);	
-
 	//Twelveth Row 
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[36],0,11,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[37],1,11,1,1);
@@ -316,60 +305,42 @@ static void activate(GtkApplication *app, gpointer user_data){
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[39],3,11,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[40],4,11,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[41],5,11,1,1);
-
-	
 	//Thirteenth Row
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[18],3,12,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[19],4,12,1,1);
-
 	//Fourteenth Row
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[7],0,13,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[8],1,13,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[9],2,13,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[42],3,13,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[43],4,13,1,1);
-
 	//Fifteenth Row
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[4],0,14,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[5],1,14,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[6],2,14,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[13],3,14,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[14],4,14,1,1);
-
-
 	//Sixteenth Row
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[1],0,15,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[2],1,15,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[3],2,15,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[11],3,15,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[12],4,15,1,1);
-
 	//Seventeenth Row
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[20],0,16,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[21],1,16,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[22],2,16,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),ButtonLabel[23],4,16,1,1);
-
-	
 	//Eighteenth Row
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[0],0,17,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[10],1,17,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[45],2,17,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[44],3,17,1,1);
 	gtk_grid_attach(GTK_GRID(widget.grid),widget.button[16],4,17,1,1);	
-	
-
-
-
-
-
-
-
 
 
 	//Each button onclick will go the getinput function	
 	g_signal_connect(widget.button[17], "button-press-event", G_CALLBACK(Shift_Clicked), NULL);
-
 	g_signal_connect(widget.button[0],"clicked",G_CALLBACK(GetInput), NULL);
 	g_signal_connect(widget.button[1],"clicked",G_CALLBACK(GetInput), NULL);
 	g_signal_connect(widget.button[2],"clicked",G_CALLBACK(GetInput), NULL);
@@ -387,7 +358,6 @@ static void activate(GtkApplication *app, gpointer user_data){
 	g_signal_connect(widget.button[14],"clicked",G_CALLBACK(GetInput), NULL);
 	g_signal_connect(widget.button[15],"clicked",G_CALLBACK(GetInput), NULL);
 	g_signal_connect(widget.button[16],"clicked",G_CALLBACK(GetInput), NULL);
-	//g_signal_connect(widget.button[17],"clicked",G_CALLBACK(GetInput), NULL);
 	g_signal_connect(widget.button[18],"clicked",G_CALLBACK(GetInput), NULL);
 	g_signal_connect(widget.button[19],"clicked",G_CALLBACK(GetInput), NULL);
 	g_signal_connect(widget.button[20],"clicked",G_CALLBACK(GetInput), NULL);
@@ -418,8 +388,10 @@ static void activate(GtkApplication *app, gpointer user_data){
 	g_signal_connect(widget.button[45],"clicked",G_CALLBACK(GetInput), NULL);
 		
 	gtk_widget_show_all(widget.window);
-}
 
+	gtk_widget_hide(label[2]);
+	Is_Shift_Active = false;
+}
 
 int main(int argc, char **argv){
 	GtkApplication *app;
